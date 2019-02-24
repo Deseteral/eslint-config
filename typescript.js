c5e179a0ed4c6af7208a3917d6ec1992e19a5302
Deseteral <deseteral@gmail.com>
2019-02-24 17:04:41 +0100

    Fixed bad TS rule

--- typescript.js
+++ typescript.js
@@ -7,8 +7,12 @@ module.exports = Object.assign({}, jsConfig, {
   },
   plugins: ['@typescript-eslint'],
   rules: Object.assign({}, jsConfig.rules, {
+    // Rules handled by TS type checker
     'no-unused-vars': 'off',
     'no-undef': 'off',
     'import/no-unresolved': 'off',
+
+    // Rules that don't work well with TypeScript
+    'lines-between-class-members': 'off',
   }),
 });
